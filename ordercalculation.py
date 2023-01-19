f08bf75ee09cd841b1ea205eaa05c021320b8087
Muhammed Bostancı <93875236+bostanci58@users.noreply.github.com>
2023-01-19 23:21:44 +0300

    Update ordercalculation.py

--- ordercalculation.py
+++ ordercalculation.py
@@ -1,4 +1,4 @@
-order1 = int(input("1. order price : "))
-order2 = int(input("2. order price : "))
-order3 = int(input("3. order price : "))
+order1 = float(input("1. order price : "))
+order2 = float(input("2. order price : "))
+order3 = float(input("3. order price : "))
 print(order1 + order2 + order3)
